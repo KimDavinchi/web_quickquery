76d8f4cefa48ec738b31c8f72636bf37389729a1
kimdavinchi <kdbdml@gmail.com>
2026-02-15 14:33:56 +0900

    Feat: Add Vercel Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from "@vercel/analytics/react";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
@@ -45,6 +46,7 @@ export default function RootLayout({
       >
         {children}
         <GoogleAdsense pId={process.env.NEXT_PUBLIC_GOOGLE_ADSENSE_ID || ""} />
+        <Analytics />
       </body>
     </html>
   );
